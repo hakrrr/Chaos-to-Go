a13c7fe3ddd5ccdec21392813d3a18af08d05e50
PainaPuruPuru <dorota.gaska@tum.de>
2021-06-05 22:57:53 +0200

    Basic conveyor belt models and textures added, with movement

---
+++ Chaos to Go/Assets/Scripts/ScrollTexture.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollTexture : MonoBehaviour
+{
+    //public float scrollX = 0f;
+    public float scrollY = -0.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float offsetX = Time.time * scrollX;
+        float offsetY = Time.time * scrollY;
+
+        GetComponent<Renderer>().material.SetFloat("_TexOffsetY", offsetY);
+    }
+}
